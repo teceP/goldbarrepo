6996d4ca44b1ae3f78f1b94f96150d42b7bd2147
mtek <mario.teklic77@googlemail.com>
2019-04-27 21:25:08 +0200

    splashscreen, first buttons

---
+++ app/src/main/java/com/example/goldbarlift/splash.java
@@ -0,0 +1,43 @@
+package com.example.goldbarlift;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myAnimation = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myAnimation);
+        iv.startAnimation(myAnimation);
+        final Intent i = new Intent(this, mainScreen.class);
+        Thread timer = new Thread(){
+            public void run() {
+                try {
+                    sleep(2500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    startActivity(i);
+                    finish();
+            }
+
+            }
+        };
+        timer.start();
+
+    }
+}
