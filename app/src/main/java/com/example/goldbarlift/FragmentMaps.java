c1f78b860f49154db3302ae3db4e9ac6efb3632d
mtek <mario.teklic77@googlemail.com>
2019-06-04 00:31:31 +0200

    google maps läuft

--- app/src/main/java/com/example/goldbarlift/FragmentMaps.java
+++ app/src/main/java/com/example/goldbarlift/FragmentMaps.java
@@ -1,5 +1,7 @@
 package com.example.goldbarlift;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,10 +16,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FragmentMaps extends Fragment implements OnMapReadyCallback {
+public class FragmentMaps extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, LocationListener {
     private SupportMapFragment supportMapFragment;
     private GoogleMap gmap;
 
@@ -44,9 +47,41 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        // HIER AKTUELLEN STANDORT NEHMEN
+        LatLng sydney = new LatLng(-34, 20);
         gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        gmap.setOnMapLongClickListener(this);
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        gmap.clear();
+        gmap.addMarker(new MarkerOptions()
+                .position(point)
+                .title("Your Event Here!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
